862c51cba508c5f50f3aa584b6959c0c5d60a777
Apo Kong <apokong@gmail.com>
2021-04-09 08:53:24 +0800

    backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import * as React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-import CustomButton from './components/CustomButton';
+import * as React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import CustomButton from "./components/CustomButton";
 
 const styles = StyleSheet.create({
-    box: { padding: 10 },
-    text: { fontWeight: 'bold' }
-  });
-
+  box: { padding: 10 },
+  text: { fontWeight: "bold" },
+});
 
 class App extends React.Component {
-    render() {
-      return (
-        <View style={styles.box}>
-          <Text style={styles.text}>Hello, world!</Text>
-          <CustomButton />
-        </View>
-      );
-    }
+  render() {
+    return (
+      <View style={styles.box}>
+        <Text style={styles.text}>Hello, world!</Text>
+        <CustomButton />
+      </View>
+    );
   }
+}
 
 export default App;
